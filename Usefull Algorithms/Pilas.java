7df0bc5622c22da98eb6e200b1b497397e90dd1f
VirtualEagle <86083041+VirtualEagle04@users.noreply.github.com>
2023-02-20 20:16:29 -0500

    Upload Pilas.java

---
+++ Usefull Algorithms/Pilas.java
@@ -0,0 +1,18 @@
+public class Pilas {
+	public static void main(String[] args) {
+		Stack<Integer> pila = new Stack<Integer>();
+		
+		Random r = new Random();
+		for (int i = 0; i < 10; i++) {
+			int siguiente = r.nextInt(400);
+			System.out.println(siguiente);
+			pila.push(siguiente);
+		}
+		System.out.println(pila.peek());
+		System.out.println("------------");
+		while (!pila.isEmpty()) {
+			int actual = pila.pop();
+			System.out.println(actual);
+		}
+	}
+}
